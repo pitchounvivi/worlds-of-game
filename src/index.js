4ee9bb09794a1e21ca3d9a8d18031c4b1c87e8dd
pitchounvivi <pitchounvivi@gmail.com>
2019-11-01 15:37:26 +0100

    Add LoginFormComponent

--- src/index.js
+++ src/index.js
@@ -1,14 +1,19 @@
 import { LogoComponent } from "./shared/component/logo.component";
 import { TitleComponent } from "./shared/component/title.component";
 import { RegisterButtonComponent } from "./shared/component/register-button.component";
+import { LoginFormComponent } from "./shared/component/login-form.component";
+import { User } from "./shared/models/user.models";
 
 
+const user = new User("John", "secret");
 
 const parent = document.createElement("div");
 
 new LogoComponent(parent);
 
 new TitleComponent(parent, "Game Session / Worlds of Game");
+
+new LoginFormComponent(parent,user);
 
 new RegisterButtonComponent(parent,"Register");
 
--- www/dist/index.js
+++ www/dist/index.js
@@ -94,7 +94,19 @@
 /***/ (function(module, __webpack_exports__, __webpack_require__) {
 
 "use strict";
-eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _shared_component_logo_component__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./shared/component/logo.component */ \"./src/shared/component/logo.component.js\");\n/* harmony import */ var _shared_component_title_component__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./shared/component/title.component */ \"./src/shared/component/title.component.js\");\n\n\nvar parent = document.createElement(\"div\");\nnew _shared_component_logo_component__WEBPACK_IMPORTED_MODULE_0__[\"LogoComponent\"](parent);\nnew _shared_component_title_component__WEBPACK_IMPORTED_MODULE_1__[\"TitleComponent\"](parent, \"Game Session / Worlds of Game\");\ndocument.body.appendChild(parent);\n\n//# sourceURL=webpack:///./src/index.js?");
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _shared_component_logo_component__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./shared/component/logo.component */ \"./src/shared/component/logo.component.js\");\n/* harmony import */ var _shared_component_title_component__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./shared/component/title.component */ \"./src/shared/component/title.component.js\");\n/* harmony import */ var _shared_component_register_button_component__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ./shared/component/register-button.component */ \"./src/shared/component/register-button.component.js\");\n/* harmony import */ var _shared_component_login_form_component__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(/*! ./shared/component/login-form.component */ \"./src/shared/component/login-form.component.js\");\n/* harmony import */ var _shared_models_user_models__WEBPACK_IMPORTED_MODULE_4__ = __webpack_require__(/*! ./shared/models/user.models */ \"./src/shared/models/user.models.js\");\n\n\n\n\n\nvar user = new _shared_models_user_models__WEBPACK_IMPORTED_MODULE_4__[\"User\"](\"John\", \"secret\");\nvar parent = document.createElement(\"div\");\nnew _shared_component_logo_component__WEBPACK_IMPORTED_MODULE_0__[\"LogoComponent\"](parent);\nnew _shared_component_title_component__WEBPACK_IMPORTED_MODULE_1__[\"TitleComponent\"](parent, \"Game Session / Worlds of Game\");\nnew _shared_component_login_form_component__WEBPACK_IMPORTED_MODULE_3__[\"LoginFormComponent\"](parent, user);\nnew _shared_component_register_button_component__WEBPACK_IMPORTED_MODULE_2__[\"RegisterButtonComponent\"](parent, \"Register\");\ndocument.body.appendChild(parent);\n\n//# sourceURL=webpack:///./src/index.js?");
+
+/***/ }),
+
+/***/ "./src/shared/component/login-form.component.js":
+/*!******************************************************!*\
+  !*** ./src/shared/component/login-form.component.js ***!
+  \******************************************************/
+/*! exports provided: LoginFormComponent */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, \"LoginFormComponent\", function() { return LoginFormComponent; });\n/* harmony import */ var _models_user_models__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ../models/user.models */ \"./src/shared/models/user.models.js\");\nfunction _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError(\"Cannot call a class as a function\"); } }\n\n\nvar LoginFormComponent = function LoginFormComponent(parent, user) {\n  _classCallCheck(this, LoginFormComponent);\n\n  var setAttributesTo = function setAttributesTo(elem, attributes) {\n    for (var key in attributes) {\n      elem.setAttribute(key, attributes[key]);\n    }\n  };\n\n  var containerElement = document.createElement(\"div\");\n  var formElement = document.createElement(\"form\");\n  var emailElement = document.createElement(\"input\");\n  var passwordElement = document.createElement(\"input\");\n  var buttonElement = document.createElement(\"button\");\n  var buttonText = document.createTextNode(\"Go\");\n  setAttributesTo(formElement, {\n    method: \"post\",\n    action: \"\"\n  });\n  setAttributesTo(emailElement, {\n    type: \"mail\",\n    placeholder: \"email\",\n    value: user.login\n  });\n  setAttributesTo(passwordElement, {\n    type: \"password\",\n    placeholder: \"password\",\n    value: user.password\n  });\n  formElement.appendChild(emailElement);\n  formElement.appendChild(passwordElement);\n  buttonElement.appendChild(buttonText);\n  formElement.appendChild(buttonElement);\n  containerElement.appendChild(formElement);\n  parent.appendChild(containerElement);\n};\n\n//# sourceURL=webpack:///./src/shared/component/login-form.component.js?");
 
 /***/ }),
 
@@ -110,6 +122,18 @@ eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export (binding) *
 
 /***/ }),
 
+/***/ "./src/shared/component/register-button.component.js":
+/*!***********************************************************!*\
+  !*** ./src/shared/component/register-button.component.js ***!
+  \***********************************************************/
+/*! exports provided: RegisterButtonComponent */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, \"RegisterButtonComponent\", function() { return RegisterButtonComponent; });\nfunction _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError(\"Cannot call a class as a function\"); } }\n\nvar RegisterButtonComponent = function RegisterButtonComponent(parent, name) {\n  _classCallCheck(this, RegisterButtonComponent);\n\n  var containerElement = document.createElement(\"div\");\n  var registerButton = document.createElement(\"button\");\n  var registerButtonText = document.createTextNode(name);\n  var attributes = {\n    type: \"submit\",\n    value: registerButtonText\n  };\n\n  var setAttributesTo = function setAttributesTo(registerButton, attributes) {\n    for (var key in attributes) {\n      registerButton.setAttribute(key, attributes[key]);\n    }\n  };\n\n  registerButton.appendChild(registerButtonText);\n  containerElement.appendChild(registerButton);\n  parent.appendChild(containerElement);\n};\n\n//# sourceURL=webpack:///./src/shared/component/register-button.component.js?");
+
+/***/ }),
+
 /***/ "./src/shared/component/title.component.js":
 /*!*************************************************!*\
   !*** ./src/shared/component/title.component.js ***!
@@ -119,6 +143,18 @@ eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export (binding) *
 
 "use strict";
 eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, \"TitleComponent\", function() { return TitleComponent; });\nfunction _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError(\"Cannot call a class as a function\"); } }\n\nvar TitleComponent = function TitleComponent(parent, title) {\n  _classCallCheck(this, TitleComponent);\n\n  var containerElement = document.createElement(\"div\");\n  var titleElement = document.createElement(\"h1\");\n  var titleText = document.createTextNode(title);\n  titleElement.appendChild(titleText);\n  containerElement.appendChild(titleElement);\n  parent.appendChild(containerElement);\n};\n\n//# sourceURL=webpack:///./src/shared/component/title.component.js?");
+
+/***/ }),
+
+/***/ "./src/shared/models/user.models.js":
+/*!******************************************!*\
+  !*** ./src/shared/models/user.models.js ***!
+  \******************************************/
+/*! exports provided: User */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, \"User\", function() { return User; });\nfunction _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError(\"Cannot call a class as a function\"); } }\n\nvar User = function User(login, password, firstName, lastName, mail, phone, address, city, zipCode) {\n  _classCallCheck(this, User);\n\n  this.login = login; //a changer par surname\n\n  this.password = password;\n  this.firstName = firstName;\n  this.lastName = lastName;\n  this.mail = mail;\n  this.phone = phone;\n  this.address = address;\n  this.city = city;\n  this.zipCode = zipCode;\n};\n\n//# sourceURL=webpack:///./src/shared/models/user.models.js?");
 
 /***/ })
 
