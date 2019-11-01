91c67c0212a3677237bf696de69da4213e5f1e45
pitchounvivi <pitchounvivi@gmail.com>
2019-11-01 18:09:23 +0100

    Create Component (used by many others)

---
+++ src/shared/components/component.js
@@ -0,0 +1,12 @@
+export class Component {
+
+    constructor() {
+        
+    }
+
+    setAttributesTo(element, attributes) {
+        for (const key in attributes) {
+            element.setAttribute(key, attributes[key]);
+        }
+    }
+}
