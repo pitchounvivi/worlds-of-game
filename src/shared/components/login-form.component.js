1bd8bae542aa9eea57c782582b642a130d297dc9
pitchounvivi <pitchounvivi@gmail.com>
2019-11-01 17:17:28 +0100

    Create method display in LoginFormComponent

--- src/shared/components/login-form.component.js
+++ src/shared/components/login-form.component.js
@@ -1,20 +1,26 @@
-
 export class LoginFormComponent{
 
-    constructor(parent,user){
+    constructor(user, loginName) {
+        this.value = user;   
+        this.name = loginName;    
+    }
+
+    display(parent){
         const setAttributesTo = (elem, attributes) => {
             for (const key in attributes) {
                 elem.setAttribute(key, attributes[key]);
             }
         }
 
+        
         const containerElement = document.createElement("div");
         const formElement = document.createElement("form");
         const emailElement = document.createElement("input");
         const passwordElement = document.createElement("input");
         const buttonElement = document.createElement("button");
-        let buttonText = document.createTextNode("Go");
+        let buttonText = document.createTextNode(this.name);
 
+        
         setAttributesTo(formElement, {
             method: "post",
             action: ""
@@ -23,13 +29,18 @@ export class LoginFormComponent{
         setAttributesTo(emailElement, {
             type: "mail",
             placeholder: "email",
-            value: user.login
+            value: this.user.login
         })
 
         setAttributesTo(passwordElement, {
             type: "password",
             placeholder: "password",
-            value: user.password  
+            value: this.user.password  
+        })
+
+        setAttributesTo(buttonElement, {
+            type: "submit",
+            name: this.name
         })
 
         formElement.appendChild(emailElement)
