1f6c932304d68144e2b38af65d517424d699cd75
pitchounvivi <pitchounvivi@gmail.com>
2019-11-01 15:06:08 +0100

    Add register Button in view

--- src/index.js
+++ src/index.js
@@ -1,10 +1,15 @@
 import { LogoComponent } from "./shared/component/logo.component";
 import { TitleComponent } from "./shared/component/title.component";
+import { RegisterButtonComponent } from "./shared/component/register-button.component";
+
+
 
 const parent = document.createElement("div");
 
 new LogoComponent(parent);
 
 new TitleComponent(parent, "Game Session / Worlds of Game");
+
+new RegisterButtonComponent(parent,"Register");
 
 document.body.appendChild(parent);
