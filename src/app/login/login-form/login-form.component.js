647049afe7799acbe614074786b2acb833d2f670
pitchounvivi <pitchounvivi@gmail.com>
2019-11-02 11:27:39 +0100

    Fix codacy

--- src/app/login/login-form/login-form.component.js
+++ src/app/login/login-form/login-form.component.js
@@ -20,27 +20,27 @@ export class LoginFormComponent extends Component{
         this.setAttributesTo(formElement, {
             method: "post",
             action: ""
-        })
+        });
 
         this.setAttributesTo(emailElement, {
             type: "mail",
             placeholder: "email",
             value: this.user.login
-        })
+        });
 
         this.setAttributesTo(passwordElement, {
             type: "password",
             placeholder: "password",
             value: this.user.password  
-        })
+        });
 
         this.setAttributesTo(buttonElement, {
             type: "submit",
             name: this.name
-        })
+        });
 
-        formElement.appendChild(emailElement)
-        formElement.appendChild(passwordElement)
+        formElement.appendChild(emailElement);
+        formElement.appendChild(passwordElement);
         buttonElement.appendChild(buttonText);
         formElement.appendChild(buttonElement);
         containerElement.appendChild(formElement);
