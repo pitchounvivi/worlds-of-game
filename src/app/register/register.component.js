fc8390c176c464f248d122e0314fb0e544a32eb7
pitchounvivi <pitchounvivi@gmail.com>
2019-11-02 11:16:22 +0100

    Add registerForm.display in RegisterComponent

--- src/app/register/register.component.js
+++ src/app/register/register.component.js
@@ -1,15 +1,19 @@
 import { TitleComponent } from "../../shared/components/title/title.component";
-import { User } from "../../shared/models/user.model";
+//import { User } from "../../shared/models/user.model";
+import { RegisterFormComponent } from "./register-form/register-form.component";
 
 export class RegisterComponent {
 
-    constructor(){
+    constructor(user){
+        this.user = user;
         this.title = new TitleComponent("Register");
+        this.registerForm = new RegisterFormComponent(this.user);
     }
 
     display(){
         const parent = document.createElement("div");
         this.title.display(parent);
+        this.registerForm.display(parent);
 
         document.body.appendChild(parent);
     }
