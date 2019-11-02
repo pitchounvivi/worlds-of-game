c048ce9dd0124ccbe418b054864e548789a222e8
pitchounvivi <pitchounvivi@gmail.com>
2019-11-02 11:09:41 +0100

    Add basic form in RegisterFormeComponent

--- src/app/register/register-form/register-form.component.js
+++ src/app/register/register-form/register-form.component.js
@@ -1,17 +1,37 @@
 import { Component } from "../../../shared/components/component";
+import { InputComponent } from "../../../shared/components/input/input.component";
 
-export class RegisterFormComponent extends Component{
+export class RegisterFormComponent extends Component {
 
-    constructor(login,password, firstName,lastName,mail,phone,address,city,zipCode){
-        this.login = login; //a changer par surname ?
-        this.password = password;
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.mail = mail;
-        this.phone = phone;
-        this.address = address;
-        this.city = city;
-        this.zipCode = zipCode;
+    constructor(user) {
+        super();
+        this.user = user;
+    }
+
+    display(parent) {
+        const containerElement = document.createElement("div");
+        const formElement = document.createElement("form");
+        const loginZone = document.createElement("div");
+        const loginLabel = document.createElement("label");
+        const loginInput = document.createElement("input");
+
+        this.setAttributesTo(formElement, {
+            method: "post",
+            action: ""
+        })
+
+        this.setAttributesTo(loginInput, {
+            type: "mail",
+            placeholder: "email",
+            value: this.user.login
+        });
+
+        loginLabel.appendChild(document.createTextNode("login : "))
+        loginZone.appendChild(loginLabel);
+        loginZone.appendChild(loginInput);
+        formElement.appendChild(loginZone);
+        containerElement.appendChild(formElement);
+        parent.appendChild(containerElement);
     }
 
 }
