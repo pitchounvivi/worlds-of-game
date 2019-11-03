faf732652eb64449f0f011c8f8b483c33ef6b5ed
pitchounvivi <pitchounvivi@gmail.com>
2019-11-04 00:02:06 +0100

    ReturnButtonComponent unused

--- src/app/search-game/search-game.component.js
+++ src/app/search-game/search-game.component.js
@@ -1,17 +1,20 @@
 import { TitleComponent } from "../../shared/components/title/title.component";
 import { SearchGameFormComponent } from "./search-game-form/search-game-form.component";
+//import { ReturnButtonComponent } from "../../shared/components/return-button/return-button.component";
 
 export class SearchGameComponent{
     constructor(user){
         this.user = user;
         this.title = new TitleComponent("Search Game");
         this.searchGameForm = new SearchGameFormComponent(this.user);
+        //.returnButton = new ReturnButtonComponent("Return");
     }
 
     display(){
         const parent = document.createElement("div");
         this.title.display(parent);
         this.searchGameForm.display(parent);
+        //this.returnButton.display(parent);
 
         document.body.appendChild(parent);
     }
