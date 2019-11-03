e1c043797e63a816dc4ac56c20c0f66ffaf6eb29
pitchounvivi <pitchounvivi@gmail.com>
2019-11-03 23:52:10 +0100

    Adding user and form in SearchGameComponent

--- src/app/search-game/search-game.component.js
+++ src/app/search-game/search-game.component.js
@@ -1,14 +1,18 @@
 import { TitleComponent } from "../../shared/components/title/title.component";
+import { SearchGameFormComponent } from "./search-game-form/search-game-form.component";
 
 export class SearchGameComponent{
-    constructor(){
+    constructor(user){
+        this.user = user;
         this.title = new TitleComponent("Search Game");
-        }
+        this.searchGameForm = new SearchGameFormComponent(this.user);
+    }
 
     display(){
         const parent = document.createElement("div");
         this.title.display(parent);
-        
+        this.searchGameForm.display(parent);
+
         document.body.appendChild(parent);
     }
 }
