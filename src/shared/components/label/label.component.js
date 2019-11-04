d25b0a7740d419b9a19aadb2662d0af246a2778f
pitchounvivi <pitchounvivi@gmail.com>
2019-11-04 15:47:00 +0100

    Create LabelComponent

--- src/shared/components/label/label.component.js
+++ src/shared/components/label/label.component.js
@@ -1,19 +1,13 @@
 export class LabelComponent {
 
-    constructor(text, attributes) {
-        this.text = text;
-        this.attributes = attributes;
+    constructor(text) {
+        this.text = text;     
     }
 
     display(parent) {
         const label = document.createElement("label");
         label.appendChild(document.createTextNode(this.text));
 
-        for (const key in this.attributes) {
-            if (this.attributes.hasOwnProperty(key)) {
-                label.setAttribute(key, this.attributes[key]);
-            }
-        }
         parent.appendChild(label);
     }
 }
