bab8024f4a9593ebf1af5a07dbf1896814dc796c
pitchounvivi <pitchounvivi@gmail.com>
2019-11-04 16:09:14 +0100

    Modifying InputComp to handle the label display

--- src/shared/components/input/input.component.js
+++ src/shared/components/input/input.component.js
@@ -1,17 +1,33 @@
+import { LabelComponent } from "../label/label.component";
+
 export class InputComponent {
 
     constructor(attributes) {
         this.attributes = attributes;
+        this.label = null;
         this.input = null;
+
+        if (attributes.label) {
+            this.label = new LabelComponent(attributes.label, attributes.label.value);
+        }
     }
 
     display(parent) {
         this.input = document.createElement("input");
         for (const key in this.attributes) {
+            if (key === "label"){
+                continue; //continue permet de sauter la ligne et passe à la suite des arguments
+            }
             if (this.attributes.hasOwnProperty(key)) {
                 this.input.setAttribute(key, this.attributes[key]);
             }
         }
+
+        if(this.label){
+            this.label.display(parent);
+        }
+        
+
         parent.appendChild(this.input);
     }
 
