6d69b4fd0226632cd98e5d5c61578ca56e0adef6
pitchounvivi <pitchounvivi@gmail.com>
2019-11-05 14:34:20 +0100

    Post request creation in the UserService

--- src/shared/services/user.service.js
+++ src/shared/services/user.service.js
@@ -1,11 +1,25 @@
 import { User } from "../models/user.model";
+import $ from "jquery";
 
 const user = new User("John", "secret");
 
-export class UserService{
+export class UserService {
 
-    static get(){
+    static get() {
         return user;
+    }
+
+    static post() {
+        //on formule une requête
+        $.ajax({
+            url: "http://worldsofgame.alwaysdata.net/user.php",
+            method: "POST",
+            contentType: "application/json",
+            data: JSON.stringify(this.user),
+            dataType: "json" //conversion automatique de la reponse en json
+        });
+
+        return true; // cette ligne permet le renvoi du résultat de $.ajax
     }
 
 }
