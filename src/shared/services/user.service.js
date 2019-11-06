941acb5389c8d4c3ec6bd9d2c6ce761e9f82b59f
pitchounvivi <pitchounvivi@gmail.com>
2019-11-06 16:21:37 +0100

    Fixed an error sending the user

--- src/shared/services/user.service.js
+++ src/shared/services/user.service.js
@@ -11,11 +11,12 @@ export class UserService {
 
     static post() {
         // le return permet de renvoyer tout le $.ajax
+        
         return $.ajax({
             url: "http://worldsofgame.alwaysdata.net/user.php",
             method: "POST",
             contentType: "application/json",
-            data: JSON.stringify(this.user),
+            data: JSON.stringify(user),
             dataType: "json" //conversion automatique de la reponse en json
         });
     }
