0cd57031b5d1f311cba0bb15adb6ffd42cce7010
pitchounvivi <pitchounvivi@gmail.com>
2019-11-07 09:33:15 +0100

    Editing index.js to display LoginComponent for TP

--- src/index.js
+++ src/index.js
@@ -1,23 +1,23 @@
 
 // import { SearchGameComponent } from "./app/search-game/search-game.component";
-//import { LoginComponent } from "./app/login/login.component";
-import { RegisterComponent } from "./app/register/register.component";
+import { LoginComponent } from "./app/login/login.component";
+//import { RegisterComponent } from "./app/register/register.component";
 
 
 
 const parent = document.createElement("div");
 
-// let loginComponent = new LoginComponent();
-// loginComponent.src = "assets/images/logo/logo.png";
-// loginComponent.alt = "Le LOGO de l'application";
-// loginComponent.text = "Game Session / Worlds of Game";
-// loginComponent.loginName = "Go";
-// loginComponent.registerName = "Register";
-// loginComponent.user = user;
-// loginComponent.display(parent);
+let loginComponent = new LoginComponent();
+loginComponent.src = "assets/images/logo/logo.png";
+loginComponent.alt = "Le LOGO de l'application";
+loginComponent.text = "Game Session / Worlds of Game";
+loginComponent.loginName = "Go";
+loginComponent.registerName = "Register";
+//loginComponent.user = user;
+loginComponent.display(parent);
 
-const registerComponent = new RegisterComponent();
-registerComponent.display(parent);
+// const registerComponent = new RegisterComponent();
+// registerComponent.display(parent);
 
 // const searchGameComponent = new  SearchGameComponent(user);
 // searchGameComponent.display(parent);
