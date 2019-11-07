971ef784900a81e6525c8969cde990cee98ac314
pitchounvivi <pitchounvivi@gmail.com>
2019-11-07 12:48:45 +0100

    Display text and buttons Component

--- src/app/home/home.component.js
+++ src/app/home/home.component.js
@@ -1,21 +1,37 @@
 import { LogoComponent } from "../../shared/components/logo/logo.component";
 import { TitleComponent } from "../../shared/components/title/title.component";
+import { ButtonComponent } from "../../shared/components/button/button.component";
+import { ZoneDisplayComponent } from "../../shared/components/zone-display/zone-display.component";
 
-export class HomeComponent{
+export class HomeComponent {
 
-    constructor(){
-        this.logo = new LogoComponent("assets/images/logo3.png","Le LOGO de l'application");
+    constructor() {
+        this.logo = new LogoComponent("assets/images/logo3.png", "Le LOGO de l'application");
         this.title = new TitleComponent("Welcome to Game Session Society");
+        this.question = new ZoneDisplayComponent("Ready for a Game Party ?");
+        this.proposeButton = new ButtonComponent(
+            "Proposed Game",
+            {
+                type: "submit",
+                value: "propose"
+            }
+        );
+        this.searchButton = new ButtonComponent(
+            "Search Game",
+            {
+                type: "submit",
+                value: "search"
+            }
+        );
     }
 
-    display(parent){
+    display(parent) {
         this.logo.display(parent);
         this.title.display(parent);
-
-        let question = document.createElement("p");
-        question = document.createTextNode("Ready for a Game Party ?");
-
-        parent.appendChild(question);
+        this.question.display(parent);
+        this.proposeButton.display(parent);
+        this.searchButton.display(parent);
+        
         document.body.appendChild(parent);
     }
 }
