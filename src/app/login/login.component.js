9098ec56aa6f1a580573188fa4904e59c380b25e
pitchounvivi <pitchounvivi@gmail.com>
2019-11-07 09:48:58 +0100

    Display TitleComponent for TP

--- src/app/login/login.component.js
+++ src/app/login/login.component.js
@@ -1,38 +1,49 @@
-import { LogoComponent } from "../../shared/components/logo/logo.component";
+//import { LogoComponent } from "../../shared/components/logo/logo.component";
 import { TitleComponent } from "../../shared/components/title/title.component";
-import { LoginFormComponent } from "./login-form/login-form.component";
-import { RegisterButtonComponent } from "./register-button/register-button.component";
+//import { LoginFormComponent } from "./login-form/login-form.component";
 
 
 export class LoginComponent {
 
     constructor() {
-        this.src = "?";
-        this.alt = "?";
-        this.text = "?";
-        this.loginName = "?";
-        this.registerName = "?";
-        this.user = "?";
+        // this.src = "?";
+        // this.alt = "?";
+        // this.text = "?";
+        // this.loginName = "?";
+        // this.registerName = "?";
+        // this.user = "?";
+
+        //this.logo = new LogoComponent("assets/images/logo/logo.png","Le LOGO de l'application");
+        this.title = new TitleComponent("Game session / Worlds of Game");
+        //this.loginForm = new LoginFormComponent();
+
     }
 
     display(parent){
-        let logoComponent = new LogoComponent();
-        logoComponent.src = this.src;
-        logoComponent.alt = this.alt;
-        logoComponent.display(parent);
+        // let logoComponent = new LogoComponent();
+        // logoComponent.src = this.src;
+        // logoComponent.alt = this.alt;
+        // logoComponent.display(parent);
 
-        let titleComponent = new TitleComponent();
-        titleComponent.text =  this.text;
-        titleComponent.display(parent);
+        // let titleComponent = new TitleComponent();
+        // titleComponent.text =  this.text;
+        // titleComponent.display(parent);
 
-        let loginFormComponent = new LoginFormComponent();
-        loginFormComponent.name = this.loginName;
-        loginFormComponent.user = this.user;
-        loginFormComponent.display(parent);
+        // let loginFormComponent = new LoginFormComponent();
+        // loginFormComponent.name = this.loginName;
+        // loginFormComponent.user = this.user;
+        // loginFormComponent.display(parent);
 
-        let registerButtonComponent = new RegisterButtonComponent();
-        registerButtonComponent.name = this.registerName;
-        registerButtonComponent.display(parent);
+        // let registerButtonComponent = new RegisterButtonComponent();
+        // registerButtonComponent.name = this.registerName;
+        // registerButtonComponent.display(parent);
+
+        const enTete = document.createElement("div");
+        //this.logo.display(enTete);
+        this.title.display(parent);
+        //this.loginForm.display(parent);
+
+        document.body.appendChild(parent);
     }
 
 }
