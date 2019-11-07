00a1ae80814618d33586810ff454579f181b8825
pitchounvivi <pitchounvivi@gmail.com>
2019-11-07 09:47:58 +0100

    Display LoginComponent for TP

--- src/index.js
+++ src/index.js
@@ -1,19 +1,18 @@
-
 // import { SearchGameComponent } from "./app/search-game/search-game.component";
 import { LoginComponent } from "./app/login/login.component";
 //import { RegisterComponent } from "./app/register/register.component";
 
 
-
 const parent = document.createElement("div");
 
-let loginComponent = new LoginComponent();
-loginComponent.src = "assets/images/logo/logo.png";
-loginComponent.alt = "Le LOGO de l'application";
-loginComponent.text = "Game Session / Worlds of Game";
-loginComponent.loginName = "Go";
-loginComponent.registerName = "Register";
+// let loginComponent = new LoginComponent();
+// loginComponent.src = "assets/images/logo/logo.png";
+// loginComponent.alt = "Le LOGO de l'application";
+// loginComponent.text = "Game Session / Worlds of Game";
+// loginComponent.loginName = "Go";
+// loginComponent.registerName = "Register";
 //loginComponent.user = user;
+const loginComponent = new LoginComponent();
 loginComponent.display(parent);
 
 // const registerComponent = new RegisterComponent();
