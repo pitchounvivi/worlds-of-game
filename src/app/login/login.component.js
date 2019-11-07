596c776355238874513e91f90c55a7fb85a775cd
pitchounvivi <pitchounvivi@gmail.com>
2019-11-07 11:25:01 +0100

    RegisterButton display on LoginComponent for TP

--- src/app/login/login.component.js
+++ src/app/login/login.component.js
@@ -1,6 +1,7 @@
 import { TitleComponent } from "../../shared/components/title/title.component";
 import { LogoComponent } from "../../shared/components/logo/logo.component";
 import { LoginFormComponent } from "./login-form/login-form.component";
+import { RegisterButtonComponent } from "./register-button/register-button.component";
 
 export class LoginComponent {
 
@@ -8,13 +9,14 @@ export class LoginComponent {
         this.logo = new LogoComponent("../www/assets/images/logo3.png","Le LOGO de l'application");
         this.title = new TitleComponent("Game session / Worlds of Game");
         this.loginForm = new LoginFormComponent();
-
+        this.registerButton = new RegisterButtonComponent("Register");
     }
 
     display(parent){
         this.logo.display(parent);
         this.title.display(parent);
         this.loginForm.display(parent);
+        this.registerButton.display(parent);
 
         document.body.appendChild(parent);
     }
