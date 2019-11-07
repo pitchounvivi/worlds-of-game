0de76c0522073861cca962bceaa6b1f985d8a37d
pitchounvivi <pitchounvivi@gmail.com>
2019-11-07 16:53:18 +0100

    Register button delivered in the LogComp form

--- src/app/login/login.component.js
+++ src/app/login/login.component.js
@@ -1,7 +1,8 @@
 import { TitleComponent } from "../../shared/components/title/title.component";
 import { LogoComponent } from "../../shared/components/logo/logo.component";
 import { LoginFormComponent } from "./login-form/login-form.component";
-import { RegisterButtonComponent } from "./register-button/register-button.component";
+import { ButtonComponent } from "../../shared/components/button/button.component";
+import { RegisterComponent } from "../register/register.component";
 
 export class LoginComponent {
 
@@ -9,7 +10,13 @@ export class LoginComponent {
         this.logo = new LogoComponent("assets/images/logo3.png","Le LOGO de l'application");
         this.title = new TitleComponent("Game session / Worlds of Game");
         this.loginForm = new LoginFormComponent();
-        this.registerButton = new RegisterButtonComponent("Register");
+        this.registerButton = new ButtonComponent(
+            "Register",
+            {
+                type: "submit",
+                value: "register"
+            });
+        
     }
 
     display(parent){
@@ -19,6 +26,47 @@ export class LoginComponent {
         this.registerButton.display(parent);
 
         document.body.appendChild(parent);
+
+        this.registerButton.button.addEventListener(
+            "click",
+            (event) => {
+                this.clickButton(event);
+                
+            }
+        );
+    }
+
+    clickButton(event) {
+        event.preventDefault();
+
+        this.postStart();
+        this.postSuccess();
+        this.postEnd();
+    }
+
+    postStart() {
+        console.log("Start");
+        
+    }
+
+    postEnd() {
+        console.log("End");
+        
+    }
+
+    postSuccess(user) {
+        console.log("Success");
+
+        window.history.pushState(
+            {},
+            "Register Page",
+            "/Register"
+        )
+
+        document.body.innerHTML = "";
+        const register = new RegisterComponent();
+        register.display(parent);
+
     }
 
 }
