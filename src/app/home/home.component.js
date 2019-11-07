f35c121cf15a0afcd1f5fdd8a622d6a17da4588c
pitchounvivi <pitchounvivi@gmail.com>
2019-11-07 12:18:45 +0100

    Display LogoComponent and TitleComponent

--- src/app/home/home.component.js
+++ src/app/home/home.component.js
@@ -1,6 +1,17 @@
+import { LogoComponent } from "../../shared/components/logo/logo.component";
+import { TitleComponent } from "../../shared/components/title/title.component";
+
 export class HomeComponent{
 
     constructor(){
-        
+        this.logo = new LogoComponent("assets/images/logo3.png","Le LOGO de l'application");
+        this.title = new TitleComponent("Welcome to Game Session Society");
+    }
+
+    display(parent){
+        this.logo.display(parent);
+        this.title.display(parent);
+
+        document.body.appendChild(parent);
     }
 }
