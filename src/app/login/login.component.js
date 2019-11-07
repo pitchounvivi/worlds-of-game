a5478c051ac54b82a7bd651f946a4fd763269291
pitchounvivi <pitchounvivi@gmail.com>
2019-11-07 10:44:48 +0100

    Clean up unnecessary comments LoginComponent (TP)

--- src/app/login/login.component.js
+++ src/app/login/login.component.js
@@ -5,23 +5,16 @@ import { LoginFormComponent } from "./login-form/login-form.component";
 export class LoginComponent {
 
     constructor() {
-        // this.loginName = "?";
         // this.registerName = "?";
         // this.user = "?";
 
         this.logo = new LogoComponent("../www/assets/images/logo3.png","Le LOGO de l'application");
         this.title = new TitleComponent("Game session / Worlds of Game");
-        //this.loginForm = new LoginFormComponent();
         this.loginForm = new LoginFormComponent();
 
     }
 
     display(parent){
-        // let loginFormComponent = new LoginFormComponent();
-        // loginFormComponent.name = this.loginName;
-        // loginFormComponent.user = this.user;
-        // loginFormComponent.display(parent);
-
         // let registerButtonComponent = new RegisterButtonComponent();
         // registerButtonComponent.name = this.registerName;
         // registerButtonComponent.display(parent);
