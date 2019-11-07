2e94b6d09fab76e96ce40e233fe4686394381ccc
pitchounvivi <pitchounvivi@gmail.com>
2019-11-07 11:57:38 +0100

    Clean up unnecessary comments index.js for TP

--- src/index.js
+++ src/index.js
@@ -1,25 +1,15 @@
-// import { SearchGameComponent } from "./app/search-game/search-game.component";
 import { LoginComponent } from "./app/login/login.component";
 //import { RegisterComponent } from "./app/register/register.component";
 
 
 const parent = document.createElement("div");
 
-// let loginComponent = new LoginComponent();
-// loginComponent.src = "assets/images/logo/logo.png";
-// loginComponent.alt = "Le LOGO de l'application";
-// loginComponent.text = "Game Session / Worlds of Game";
-// loginComponent.loginName = "Go";
-// loginComponent.registerName = "Register";
-//loginComponent.user = user;
 const loginComponent = new LoginComponent();
 loginComponent.display(parent);
 
 // const registerComponent = new RegisterComponent();
 // registerComponent.display(parent);
 
-// const searchGameComponent = new  SearchGameComponent(user);
-// searchGameComponent.display(parent);
 
 document.body.appendChild(parent);
 
