fbe96dc76c1bdcdf82f784a530a218978dcc4f69
pitchounvivi <pitchounvivi@gmail.com>
2019-11-07 12:00:46 +0100

    Editing index.js to display HomeComponent for TP

--- src/index.js
+++ src/index.js
@@ -1,15 +1,18 @@
-import { LoginComponent } from "./app/login/login.component";
+//import { LoginComponent } from "./app/login/login.component";
 //import { RegisterComponent } from "./app/register/register.component";
+import { HomeComponent } from "./app/home/home.component";
 
 
 const parent = document.createElement("div");
 
-const loginComponent = new LoginComponent();
-loginComponent.display(parent);
+// const loginComponent = new LoginComponent();
+// loginComponent.display(parent);
 
 // const registerComponent = new RegisterComponent();
 // registerComponent.display(parent);
 
+const homeComponent = new HomeComponent();
+homeComponent.display(parent);
 
 document.body.appendChild(parent);
 
